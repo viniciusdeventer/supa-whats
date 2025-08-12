cd4f036dd38e4c751b153e79a1493a660b99f223
viniciusdeventer <vinicios.miiranda5@gmail.com>
2025-08-12 20:49:30 -0300

    fix(api) Adicionado client-token da Z-API

--- app/messenger.py
+++ app/messenger.py
@@ -1,18 +1,23 @@
 import requests
-from app.config import ZAPI_INSTANCE, ZAPI_TOKEN
+from app.config import ZAPI_INSTANCE, ZAPI_TOKEN, CLIENT_TOKEN
 
 def enviar_mensagem(nome, telefone):
     mensagem = f"Olá {nome}, tudo bem com você?"
     url = f"https://api.z-api.io/instances/{ZAPI_INSTANCE}/token/{ZAPI_TOKEN}/send-text"
+
+    headers = {
+        "Client-Token": CLIENT_TOKEN,
+        "Content-Type": "application/json"
+    }
 
     payload = {
         "phone": telefone,
         "message": mensagem
     }
 
-    response = requests.post(url, json=payload)
+    response = requests.post(url, json=payload, headers=headers)
 
     if response.status_code == 200:
-        print(f"Mensagem enviada para {nome}")
+        print("Mensagens enviadas com sucesso!")
     else:
-        print(f"Erro ao enviar para {nome}: {response.text}")
+        print(f"Erro ao enviar mensagem para {nome}: {response.text}")
